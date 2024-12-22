0f597112a913f36e6dbde4d1f4ed03838de42f7c
louis5103 <louis38625103@gmail.com>
2024-12-22 23:09:15 +0900

    initialize springboot server

---
+++ src/test/java/com/project/webstack/webstack/WebstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.webstack.webstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
