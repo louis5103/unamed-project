d6f7bc6f59c986e5bb4b44332011aa8064beca1d
louis5103 <louis38625103@gmail.com>
2025-01-14 21:56:58 +0900

    springboot init

---
+++ src/main/java/com/springproject/webstack_spring/WebstackSpringApplication.java
@@ -0,0 +1,11 @@
+package com.springproject.webstack_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstackSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebstackSpringApplication.class, args);
+    }
+}
---
+++ src/test/java/com/springproject/webstack_spring/WebstackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.webstack_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstackSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
