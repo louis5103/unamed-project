5704fe9524de4bc407c07d57207a41dfd6569cf4
louis5103 <louis38625103@gmail.com>
2025-01-15 16:57:10 +0900

    logger 추가

--- src/main/java/com/springproject/webstack_spring/WebstackSpringApplication.java
+++ src/main/java/com/springproject/webstack_spring/WebstackSpringApplication.java
@@ -1,11 +1,14 @@
 package com.springproject.webstack_spring;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebstackSpringApplication {
+    private static LoggerFactory logger = LoggerFactory.getLogger(WebstackSpringApplication.class);
     public static void main(String[] args) {
+
         SpringApplication.run(WebstackSpringApplication.class, args);
     }
 }
