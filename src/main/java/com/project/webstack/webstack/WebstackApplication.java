48977266a71f3f3e42ecb80548b28ea2239374d2
aSemo3 <a_semo@naver.com>
2025-01-15 16:54:35 +0900

    123
    123이에용

--- src/main/java/com/project/webstack/webstack/WebstackApplication.java
+++ src/main/java/com/project/webstack/webstack/WebstackApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebstackApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {  //123
         SpringApplication.run(WebstackApplication.class, args);
     }
 
